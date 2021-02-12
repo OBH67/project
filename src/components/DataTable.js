cd9ee2da3b1188f2474908467df5ad40b4bcbfcd
OBH67 <omarbalanzar113@gmail.com>
2021-02-12 17:42:38 -0600

    yes

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -2,29 +2,22 @@ import React, { Component } from "react";
 import axios from "axios";
 
 const $ = require("jquery");
-require("datatables.net");
+require("datatables.net")(window, $);
 
 class Tbl extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      civilizations: [],
+      users: [],
       loading: true
     };
   }
 
-  //option 1
-  async getUsersData() {
-    const res = await axios.get("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations");
-    console.log(res.data);
-    this.setState({ loading: false, users: res.data });
-  }
-
-  /*option 2
+  //option 2
   async getUsersData1() {
     const res = await axios.get("https://jsonplaceholder.typicode.com/users");
     return res.data;
-  }*/
+  }
 
   componentDidMount() {
     this.getUsersData().then(() => this.sync());
@@ -33,8 +26,7 @@ class Tbl extends Component {
   sync() {
     this.$el = $(this.el);
     this.$el.DataTable({
-      data: this.state.users, //option 1
-      // data: this.getUsersData1(), //option 2
+      data: this.getUsersData1(), //option 2
       columns: [
         { title: "Name", data: "name" },
         { title: "Username", data: "username" },
